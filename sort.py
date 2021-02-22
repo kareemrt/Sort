745415cdd9ea3ea724c29ba5107409c003ad604c
Kareem <kareemtr19@gmail.com>
2021-02-22 13:43:05 -0800

    1.4 - Adds reversal to a sort. Factors down code for better optimization/readability

--- sort.py
+++ sort.py
@@ -7,115 +7,122 @@ import os
 import shutil
 import configparser
 
-
-table = {  "folder":"folders",
+path = os.path.expanduser('~') + '\Downloads'
+extensions = {"folder":"folders",
             "exe":"executables",
             "bat":"scripts",
             "py":"scripts",
             "zip":"archives",
             "rar":"archives",
+            "7z":"archives",
             "tar":"archives",
             "tgz":"archives",
             "iso":"disk-images",
             "pdf":"documents",
-            "docx":"documents"
+            "docx":"documents",
+            "png":"images",
+            "jpeg":"images"
             }
 
-types = ["unknown","folders","executables","scripts","archives","disk-images", "documents"]
+buckets = ["unknown","folders","executables","scripts","archives","disk-images", "documents", "images"]
+
+def makeDir(bucket): os.mkdir(bucket)
+def join(cd, target): return os.path.join(cd, target)
+def isFile(cd): return os.path.isfile(cd)
+def isDir(cd): return os.path.isdir(cd)
+def removeDir(cd): os.removedirs(cd)
+def printPath(): print("CD: " + path)
+def setPath(target): 
+    global path
+    path = target
+def doNothing(): return
 
 def sort_dir():
-    cd = os.getcwd()
-    dir_contents = os.listdir()    
-    for bucket in types:
-        if bucket not in dir_contents: os.mkdir(bucket)
+    dir_contents = os.listdir()
+    print("Sorting...")    
+    for bucket in buckets:
+        if bucket not in dir_contents: makeDir(bucket)
     for item in dir_contents:
-        path = os.path.join(cd, item)
-        if os.path.isfile(path): ext = item.rsplit(".",1)[1]
-        elif os.path.isdir(path):
-            if item in types: continue
-            ext = "folder"
-        newpath = os.path.join(cd, table.get(ext, "unknown"))
-        try:
-            shutil.move(path, newpath)
-        except shutil.Error:
-            print("No biggie")
+        temp = join(path, item) # temporary path to current file
+        if isFile(temp): 
+            try: file_ext = item.rsplit(".",1)[1] # if item is file, parse its file extension
+            except IndexError: # if file has no extension, give it a blank
+                file_ext = "" 
+        else: # item is directory
+            if item in buckets: continue # if directory is a bucket, skip
+            ext = "folder" # otherwise mark it as a folder
+
+        target = join(path, extensions.get(file_ext, "unknown")) # create new path to bucket using current file extension
+
+        try: shutil.move(temp, target) # move
+        except shutil.Error: print("File already exists in target directory") # return error if exists
+    main()
+
+def reverse():
+    dir_contents = os.listdir(path)
+    print("Reversing...")
+    for bucket in dir_contents:
+        if bucket in buckets:
+            temp = join(path, bucket)
+            bucket_contents = os.listdir(temp)
+            for item in bucket_contents:
+                item_path = join(temp, item)
+                shutil.move(item_path, path)
+            removeDir(temp)
+    main()
 
 def main():
-    print("CD: " + os.getcwd())
-    config = configparser.ConfigParser()
-    config.read('C:\Users\karee\onedrive\pywork\sort\config.ini')
-    types = config['table']
-    print(types)
-    print(str(config["table"]))
+    os.chdir(path) # change dir to path
+    printPath()
     choice = 0
-    while(choice != 1 and choice != 2 and choice != 3 and choice != 4):
-        choice = input("1. Sort CD\n2. Print CD\n3. Change CD\n4. Edit Types\n5. Exit\n")
-        choice = int (choice)
-    if choice == 1: 
-        sort_dir()
-        main()
-    if choice == 2: 
-        print_dir()
-        main()
-    if choice == 3: 
-        change_dir()
-        main()
-    if choice == 4:
-        edit()
-        main()
-    return
+    options = { "1": sort_dir, "2": print_dir, 
+                "3": change_dir, "4": edit_buckets,
+                "5": edit_ext, "6": reverse,
+                "7": doNothing}
+    while(choice not in options):
+        choice = input("1. Sort CD\n2. Print CD\n3. Change CD\n4. Edit sorted buckets\n5. Edit target filetypes\n6. Reverse Sort\n7: Exit\n")
+    options[choice]()
 
 def change_dir():
+    global path
+    printPath()
     choice = 0
-    while (choice != 1 and choice != 2 and choice != 3):
-        print("CD: " + os.getcwd())
-        choice = input("1: List Contents\n2: Change Directory\n3: Exit\n")
-        choice = int (choice)
-    if choice == 1: 
-        print_dir()
-        change_dir()
-    if choice == 2: 
-        pick_dir()
-        change_dir()
-    if choice == 3: return
-
-def pick_dir():
-    choice = 0
-    while (choice != 1 and choice != 2 and choice != 3):
+    options = {"1": "By Path", "2": "Child Dir", "3": "Parent Dir", "4": "Exit"}
+    while (choice not in options):
         choice = input("1: By Path\n2: Child Dir\n3: Parent Dir\n4: Exit\n")
-        choice = int (choice)
-    if choice == 1: 
-        path = input("Enter Path:")
-        if os.path.isdir(path): os.chdir(path)
-    elif choice == 2:
-        print_dir()
-        path = input("Enter a child dir (.. to escape)")
-        if path == "..": return
-        path = os.path.join(os.getcwd(), path)
-        if os.path.isdir(path): os.chdir(path)
-    elif choice == 3: os.chdir(os.path.split(os.getcwd())[0])
-    elif choice == 4: return
+    choice = options[choice]
 
-def print_dir(only_dir = False, only_file = False):
-    contents = os.listdir()
-    print("=\nCD: " + os.getcwd() + "\n=\n")
+    if choice == "By Path": # type path
+        temp = input("Enter Path: (.. to escape)")
+        if isDir(temp): setPath(temp)
+    elif choice == "Child Dir": # child dir
+        children = print_dir(dir_only = True, return_children=True) # list of child directories
+        
+        temp = input("Enter a child dir (.. to escape)")
+        if temp in children: setPath(join(path, temp))
+    elif choice == "Parent Dir": path = os.path.dirname(path)
+    main()
+
+def print_dir(dir_only = False, file_only = False, return_children = False): # print entire dir. can specificy to only print one type, or to return list of child dirs.
+    contents = os.listdir(path)
+    children = []
+    printPath()
     for item in contents:
-        path = os.path.join(os.getcwd(), item)
-        if only_file: 
-            if os.path.isfile(path): print("File: " + item)
-        elif only_dir:
-            if os.path.isdir(path): print("Dir: " + item)
+        temp = join(path, item)
+        if file_only: 
+            if isFile(temp): print("File: " + item)
+        elif dir_only:
+            if isDir(path): print("Dir: " + item)
+            if return_children: children.append(item) 
         else: print(item)
     print("")
+    if return_children: return children
+    main()
 
-def edit():
-    choice = 0
-    while(choice != 1 and choice != 2 and choice != 3):
-        choice = input("1. Edit Buckets\n2. Edit Extensions\n3. Exit\n")
-        choice = int (choice)
-    if choice == 1: edit_buckets() 
-    elif choice == 2: edit_ext()
-    return
+def edit(buckets=False, filetypes=False, config=False):
+    if buckets: edit_buckets()
+    elif filetypes: edit_ext()
+    main()
     
 def edit_buckets():
     choice = 0
@@ -123,50 +130,43 @@ def edit_buckets():
         choice = input("1. Add Bucket\n2. Edit Bucket\n3. Remove Bucket\n4. Exit\n")
         choice = int (choice)
 
-    if choice == 1: types.append(input("Bucket Name?\n"))
-    elif choice == 2: 
-        sel = input("Select a bucket")
-        while sel not in types:
-            sel = input("Not a bucket; select a bucket")
-        bucket = input("Rename to:\n")
-        types[types.index(sel)] = bucket
-    elif choice == 3:
-        sel = input("Select a bucket")
-        while sel not in types:
-            sel = input("Not a bucket; select a bucket")
-        types.remove(sel)
+    if choice == 1: buckets.append(input("Bucket Name: "))
+    elif choice == 2 or choice == 3: 
+        bucket = input("Select a bucket: ")
+        while bucket not in buckets:
+            bucket = input("Not a bucket; select a bucket")
+        if choice == 2:
+            temp = input("Rename to: ")
+            buckets[buckets.index(bucket)] = temp
+        elif choice == 3: buckets.remove(bucket)
     return
 
 def edit_ext():
     choice = 0
-    while(choice != 1 and choice != 2 and choice != 3 and choice != 4):
+    while(choice not in [1, 2, 3, 4]):
         choice = input("1. Add Ext\n2. Edit Ext\n3. Remove Ext\n4. Exit\n")
         choice = int (choice)
 
     if choice == 1:
-        ext = input("Type extension:\n")
-        print(types)
-        choice = input("Pick an associated filetype\n")
-        while choice not in types:
-            choice = input("Incorrect; pick an associated filetype\n")
-        table[ext] = choice
+        ext = input("Add extension: ")
+        print(buckets)
+        file_type = input("Pick an associated bucket (filetype): ")
+        while file_type not in buckets:
+            file_type = input("Incorrect; pick an associated filetype: ")
+        buckets[ext] = file_type
     if choice == 2:
-        sel = input("Select an extension\n")
-        while sel not in table.keys():
-            sel = input("Not an ext; select a file extension\n")
-        bucket = input("Rename to:\n")
-        value = table.pop(sel)
-        table[bucket] = value
-    if choice == 3:
-        for key, ans in table.items():
-            print(key+":"+ans)
-        sel = input("Select an extension\n")
-        while sel not in table.keys():
-            sel = input("Not an ext; select a file extension\n")
-        table.pop(sel)
+        for ext, buc in extensions.items():
+            print(ext+":"+buc)
+        ext = input("Select an extension\n")
+        while ext not in table.keys():
+            ext = input("Not an ext; select a file extension\n")
+        if choice == 2:
+            temp = input("Rename to:\n")
+            bucket = table.pop(ext)
+            extensions[temp] = bucket
+        elif choice == 3: table.pop(ext)
     return
         
 
 if __name__ == "__main__":
-    os.chdir("C:\\Users\karee\Downloads") # go to cd
     main()
